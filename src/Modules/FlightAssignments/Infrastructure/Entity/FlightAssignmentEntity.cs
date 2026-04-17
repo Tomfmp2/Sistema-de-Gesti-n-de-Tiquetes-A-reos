adf44400b4035233af7c10bed22deb53a846a786
Tomfmp2 <222234754+Tomfmp2@users.noreply.github.com>
2026-04-17 18:22:41 -0500

    ✨ feat(flight-assignments): Crear EntityConfiguration para asignación de roles en vuelos
    - Configurar tabla FlightAssignments con relaciones staff y flights
    - Mapear FKs a flights, staff y flight_roles
    - Crear índice compuesto único para integridad referencial

--- src/Modules/FlightAssignments/Infrastructure/Entity/FlightAssignmentEntity.cs
+++ src/Modules/FlightAssignments/Infrastructure/Entity/FlightAssignmentEntity.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using sistema_gestor_de_tiquetes_aereos.Src.Modules.Flights.Infrastructure.Entity;
+using sistema_gestor_de_tiquetes_aereos.Src.Modules.Staff.Infrastructure.Entity;
+
 namespace sistema_gestor_de_tiquetes_aereos.Src.Modules.FlightAssignments.Infrastructure.Entity;
 
 public sealed class FlightAssignmentEntity
@@ -6,4 +10,9 @@ public sealed class FlightAssignmentEntity
     public int FlightId { get; set; }
     public int StaffId { get; set; }
     public int FlightRoleId { get; set; }
+
+    // Navigation properties
+    public FlightEntity? Flight { get; set; }
+    public StaffEntity? Staff { get; set; }
+    // TODO: FlightRoleEntity - módulo FlightRoles no existe aún
 }
---
+++ src/Modules/FlightAssignments/Infrastructure/Entity/FlightAssignmentEntityConfiguration.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using sistema_gestor_de_tiquetes_aereos.Src.Modules.Flights.Infrastructure.Entity;
+using sistema_gestor_de_tiquetes_aereos.Src.Modules.Staff.Infrastructure.Entity;
+
+namespace sistema_gestor_de_tiquetes_aereos.Src.Modules.FlightAssignments.Infrastructure.Entity;
+
+public class FlightAssignmentEntityConfiguration : IEntityTypeConfiguration<FlightAssignmentEntity>
+{
+    public void Configure(EntityTypeBuilder<FlightAssignmentEntity> builder)
+    {
+        builder.ToTable("FlightAssignments");
+
+        builder.HasKey(x => x.Id);
+
+        builder
+            .Property(x => x.Id)
+            .HasColumnName("Id")
+            .HasColumnType("int")
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder
+            .Property(x => x.FlightId)
+            .HasColumnName("FlightId")
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder
+            .Property(x => x.StaffId)
+            .HasColumnName("StaffId")
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder
+            .Property(x => x.FlightRoleId)
+            .HasColumnName("FlightRoleId")
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder
+            .HasOne(x => x.Flight)
+            .WithMany()
+            .HasForeignKey(x => x.FlightId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Staff)
+            .WithMany()
+            .HasForeignKey(x => x.StaffId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // TODO: Agregar FK para FlightRoleId cuando el módulo FlightRoles esté completado
+        // builder
+        //     .HasOne<FlightRoleEntity>()
+        //     .WithMany()
+        //     .HasForeignKey(x => x.FlightRoleId)
+        //     .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => new { x.FlightId, x.StaffId, x.FlightRoleId }).IsUnique();
+    }
+}
